d5e3c48d3c9515ba53e2d1357385473ac52054eb
Milen Stanev <milen.stanev@gmail.com>
2017-09-29 18:06:45 +0300

    Add test

---
+++ src/dist.js
@@ -0,0 +1,59 @@
+"use strict";
+const fs = require('fs');
+const Builder = require('systemjs-builder');
+const pjson = require('../package.json');
+const process = require('process');
+
+let builderConfigMeta = {};
+
+for(let key in pjson.jspm.dependencies) {
+  if(!builderConfigMeta.hasOwnProperty(key)) {
+    builderConfigMeta[key] = {
+      build: false
+    };
+  }
+}
+
+/**
+ * Define baseUrl
+ * . -> build, run as npm script file
+ * .. -> debug, run as node file
+ * @type {string}
+ */
+const baseUrl = process.argv[3] || '.';
+/**
+ * Configure builder paths
+ */
+const builder = new Builder(`${baseUrl}/`, `${baseUrl}/config.js`);
+
+builder.config({
+  meta: builderConfigMeta,
+  defaultJSExtensions: true,
+  transpiler: "plugin-babel",
+  babelOptions: {
+    presets: ['babel-preset-es2017'],
+    plugins: ['babel-plugin-transform-decorators-legacy']
+  },
+  map: {
+    "systemjs-babel-build": "npm:systemjs-plugin-babel@0.0.25/systemjs-babel-browser.js"
+  }
+});
+
+builder
+  .buildStatic(
+    `${baseUrl}/src/${pjson.main}.js`,
+    `${baseUrl}/${pjson.main}.js`,
+    {
+      inject: true,
+      minify: true,
+      mangle: false,
+      sourceMaps: true,
+      format: 'umd'
+    }
+  ).then(function() {
+  console.log('Build complete\n');
+})
+  .catch(function(err) {
+    console.log('Build error\n');
+    console.log(err);
+  });
